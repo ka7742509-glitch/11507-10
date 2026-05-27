abd0b0a3ca0ed03dddef9936b701840123f58aae
BceBolod <ka7742509@gmail.com>
2026-05-27 13:01:44 +0300

    Исправил ветку домашки

--- HomeWorks/04.04.26/ConsoleApp1/ConsoleApp1/Sale.cs
+++
@@ -1,9 +0,0 @@
-namespace CoffeeShop.Models
-{
-    public class Sale
-    {
-        public DateTime Timestamp { get; set; }
-        public string DrinkName { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
